1f04a86f04e870dfb1f83cfcdfedcaaede46c422
crypt0 <acvjetan@gmail.com>
2020-09-03 10:57:19 +0200

    Add 8kyu -> Filling An Array ( Part 1 ) solution.

---
+++ 8kyu/Filling_An_Array_Part_1/filling_an_array.js
@@ -0,0 +1,11 @@
+const arr = (N) => {
+  let arr = [];
+  for (let i = 0; i < N; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+console.log(arr(5));
+console.log(arr(15));
+console.log(arr(55));
